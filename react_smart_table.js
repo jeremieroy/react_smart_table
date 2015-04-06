cda68f606ee78adf4f2b8fedab34f9b76028d410
Roy Jeremie <jeremie.roy@gmail.com>
2015-04-06 13:47:22 +0200

    set up boiler plate and UMD as seen in http://davidbcalhoun.com/2014/what-is-amd-commonjs-and-umd/

---
+++ react_smart_table.js
@@ -0,0 +1,16 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) { // AMD
+        define(['react'], factory);
+    } else if (typeof exports === 'object') { // Node, CommonJS-like
+        module.exports = factory(require('react'));
+    } else { // Browser globals (root is window)
+        root.ReactSmartTable = factory(root.React);
+    }
+}(this, function (React) {
+    "use strict";
+    var exports = {};
+    exports.testMe = function(){
+        console.log("I'm accessible !");
+    }
+    return exports;
+}));
