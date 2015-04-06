389ada13071c65060a4dda81fdb148b66531a651
Roy Jeremie <jeremie.roy@gmail.com>
2015-04-06 17:08:56 +0200

    show headers, row and columns

--- react_smart_table.js
+++ react_smart_table.js
@@ -18,19 +18,38 @@
             };
         },
         getInitialState: function() {
-            return {                
+            return {   
+                data : this.props.data // TODO: fix antipattern
             };
         },
+        getColumns: function() {
+             return Object.keys(this.state.data[0]); 
+        },
         render: function() {
+            var columns = this.getColumns();
+            
+            var tr_headers = columns.map(function (col, i) {
+                return React.DOM.th({key:i}, col);
+            });
+
+            var tr_filters = columns.map(function (col, i) {
+                return React.DOM.th({key:i}, col);
+            });
+
             var thead = React.DOM.thead( {key: "thead"}, [
-                React.DOM.tr( {key: "tr_headers"}, [/* headers */ ]),
-                (this.props.filterByColumn)?React.DOM.tr( {key: "tr_filters"}, [/* filters */ ]):null,
+                React.DOM.tr( {key: "tr_headers"}, tr_headers ),                    
+                (this.props.filterByColumn)?React.DOM.tr( {key: "tr_filters"}, tr_filters ):null
             ]);
 
-            var tbody = React.DOM.tbody( {key: "tbody"}, [
-                React.DOM.tr( {key: "1"}, [/* values 1 */ ]),
-                React.DOM.tr( {key: "2"}, [/* values 2 */ ])
-            ]);
+            var tr_body = this.state.data.map(function (row, i) {
+                return React.DOM.tr( {key:i}, 
+                    columns.map(function (col, j) {
+                        return React.DOM.td({key:j}, row[col]);
+                    })
+                )
+            });
+
+            var tbody = React.DOM.tbody( {key: "tbody"}, tr_body );
             return React.DOM.table( {}, [thead, tbody] );
         }
     });
